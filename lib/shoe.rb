ee81bc22f36099e000948710c6429e36fd23cf22
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-07-12 21:51:29 +0300

    completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def properties(brand,color,size,condition,material)
+        @brand
+        @color = color
+        @size =size
+        @condition = condition
+        @material = material
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
